c57099fcf8ad00d27dbd85f3b16f2564dc9dd87a
Tahseen Muzzafar <wartahseen32@gmail.com>
2023-02-07 14:22:35 +0530

    new file:   .gitignore new file:   README.md new file:   package-lock.json new file:   package.json new file:   public/favicon.ico 	new file:   public/index.html 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/App.test.js 	new file:   src/components/HelloWorld.jsx 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/logo.svg 	new file:   src/reportWebVitals.js 	new file:   src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Welcome to my world of react.</p>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,6 @@
+function HelloWorld(){
+    return(
+        <h3>Bye from me</h3>
+    )
+}
+export default HelloWorld
